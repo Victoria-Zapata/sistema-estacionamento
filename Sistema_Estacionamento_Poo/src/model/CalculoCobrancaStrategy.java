4a3e6c5ab84f0f90f26f11d6d8aa0e981e7dcf89
PkRchs <112730473+PkRchs@users.noreply.github.com>
2026-03-23 02:47:28 -0300

    Update CalculoCobrancaStrategy.java

--- Sistema_Estacionamento_Poo/src/model/CalculoCobrancaStrategy.java
+++ Sistema_Estacionamento_Poo/src/model/CalculoCobrancaStrategy.java
@@ -3,18 +3,3 @@ package model;
 public interface CalculoCobrancaStrategy {
     double calcular(long minutos);
 }
-
-public class CalculoTemporarioStrategy implements CalculoCobrancaStrategy {
-    @Override
-    public double calcular(long minutos) {
-        double valorHora = Estacionamento.getInstancia().getValorHoraRotativo();
-        return Math.ceil(minutos / 60.0) * valorHora;
-    }
-}
-
-public class CalculoFixoStrategy implements CalculoCobrancaStrategy {
-    @Override
-    public double calcular(long minutos) {
-        return 0.0;
-    }
-}
